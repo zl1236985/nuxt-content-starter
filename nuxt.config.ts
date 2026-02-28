df225684c32aaefa9c4c794e6926f6da29acb694
zl1236985 <46508711+zl1236985@users.noreply.github.com>
2026-02-28 15:07:04 +0800

    Update nuxt.config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  title: '我的知识分享站', // 修改为您的博客名称
+  description: '这里是分享技术与思考的地方。', // 修改为您的博客描述
 compatibilityDate: '2025-07-15',
   modules: ['@nuxt/content'],
   devtools: { enabled: true },
